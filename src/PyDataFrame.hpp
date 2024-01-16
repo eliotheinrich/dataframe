d3b4b6e6fbbcff836fdf102a2ed822f3a6635def
Eliot A Heinrich <eliot.heinrich@gmail.com>
2024-01-16 00:19:55 -0500

    can now access df metadata

--- src/PyDataFrame.hpp
+++ src/PyDataFrame.hpp
@@ -147,6 +147,7 @@ namespace dataframe {
       .def(nanobind::init<const DataFrame&>())
       .def(nanobind::init<const std::vector<uint8_t>&>())
       .def_rw("params", &DataFrame::params)
+      .def_rw("metadata", &DataFrame::metadata)
       .def_rw("slides", &DataFrame::slides)
       .def_rw("atol", &DataFrame::atol)
       .def_rw("rtol", &DataFrame::rtol)
