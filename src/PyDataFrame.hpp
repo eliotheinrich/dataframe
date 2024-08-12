3c61d1ea0f38870f1fcc467ed4708ab06d1d3e55
Eliot Heinrich <eliot.heinrich@gmail.com>
2024-08-12 11:34:18 -0400

    Updated API issues

--- src/PyDataFrame.hpp
+++ src/PyDataFrame.hpp
@@ -5,7 +5,7 @@
 #include <nanobind/stl/optional.h>
 #include <nanobind/stl/variant.h>
 #include <nanobind/stl/vector.h>
-#include <nanobind/stl/map.h>
+#include <nanobind/stl/bind_map.h>
 #include <nanobind/stl/shared_ptr.h>
 #include <nanobind/trampoline.h>
 #include <nanobind/ndarray.h>
